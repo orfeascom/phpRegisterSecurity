af7dcd49eb39f3f3fc5c4280053251d4203f58c1
orfeascom <orfeascomm@gmail.com>
2023-09-17 23:58:14 +0300

    Front validation + white listing with RegEx (email)

--- public/index.php
+++ public/index.php
@@ -1,1 +1,16 @@
 <?php
+
+require "../private/autoload.php";
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h4>This is the home page</h4>
+</body>
+</html>
