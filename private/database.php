ea1d896922ffed78284c42773c20c9850e1e4802
orfeascom <orfeascomm@gmail.com>
2023-09-23 11:17:48 +0300

    DB, Register page and sanitizing inputs

--- private/database.php
+++ private/database.php
@@ -1,1 +1,14 @@
 <?php
+
+// Parameters to connect to a database
+$dbHost = "localhost";
+$dbUser = "root";
+$dbPass = "";
+$dbName = "php_project_login";
+
+//$conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName, 33306);
+$str = "mysql:host=$dbHost;port=33306;dbname=$dbName";
+if (!$conn = new PDO($str, $dbUser, $dbPass))
+{
+    die("Databae connection failed!");
+}
