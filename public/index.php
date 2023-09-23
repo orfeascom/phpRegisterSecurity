ac5f488b491fc81e969471dfa2efad4e90241ec3
orfeascom <orfeascomm@gmail.com>
2023-09-23 16:09:11 +0300

    Final touches re: index.php

--- public/index.php
+++ public/index.php
@@ -12,13 +12,13 @@ require "../private/autoload.php";
     <title>Home</title>
 </head>
 <body>
-    <div style="float:right"><a href="logout.php">Logout</a></div>
-    <h3>This is the home page</h3>
     <?php if(check_login()): ?>
+        <div style="float:right"><a href="logout.php">Logout</a></div>
         <p>Hello <?=htmlspecialchars($_SESSION['username'])?></p>
     <?php else: ?>
         <p style="text-align:center">You need to <a href="login.php">login in</a> first!</p>
     <?php endif; ?>
+    <h3 style="text-align:center">This is the home page</h3>
 
 </body>
 </html>
